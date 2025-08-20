ef0bd5dc2bf934326dd4653a3fb633a70d20a886
Albin-MCABEE <albinanto@mcabee.in>
2025-08-20 11:22:04 +0530

    MX-194 fix confirm tab back state issue

--- src/app/controlpanel/staffManagement/page.js
+++ src/app/controlpanel/staffManagement/page.js
@@ -1,13 +1,13 @@
 import Navlink from "@/components/staffManagement/Navlink";
-import React,{ Suspense } from "react";
+import React, { Suspense } from "react";
 import Table from "@/components/staffManagement/newApplications/Table";
 
 function page() {
   return (
     <div>
       <Navlink />
-     
- <Suspense fallback={<div>Loading...</div>}>
+
+      <Suspense fallback={<div>Loading...</div>}>
         <Table />
       </Suspense>
       {/* <Table /> */}
